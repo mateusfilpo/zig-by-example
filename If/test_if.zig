0d5adc20f1523f068a23f203c58df14f9ceabb54
Mateus Filpo <mateusfilpo27@gmail.com>
2025-02-18 14:54:50 -0300

    add If examples

---
+++ If/test_if.zig
@@ -0,0 +1,59 @@
+const expect = @import("std").testing.expect;
+
+test "if expression" {
+    const a: u32 = 5;
+    const b: u32 = 4;
+    const result = if (a != b) 47 else 3089;
+    try expect(result == 47);
+}
+
+test "if boolean" {
+    const a: u32 = 5;
+    const b: u32 = 4;
+    if (a != b) {
+        try expect(true);
+    } else if (a == 9) {
+        unreachable;
+    } else {
+        unreachable;
+    }
+}
+
+test "if error union" {
+    const a: anyerror!u32 = 0;
+    if (a) |value| {
+        try expect(value == 0);
+    } else |err| {
+        _ = err;
+        unreachable;
+    }
+
+    const b: anyerror!u32 = error.BadValue;
+    if (b) |value| {
+        _ = value;
+        unreachable;
+    } else |err| {
+        try expect(err == error.BadValue);
+    }
+
+    if (a) |value| {
+        try expect(value == 0);
+    } else |_| {}
+
+    if (b) |_| {} else |err| {
+        try expect(err == error.BadValue);
+    }
+
+    var c: anyerror!u32 = 3;
+    if (c) |*value| {
+        value.* = 9;
+    } else |_| {
+        unreachable;
+    }
+
+    if (c) |value| {
+        try expect(value == 9);
+    } else |_| {
+        unreachable;
+    }
+}
